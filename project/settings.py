b63adc5c81050d9bffb583b4dd0157f52e0e6875
krijal <krijalsuwal67@gmail.com>
2025-03-09 17:20:00 +0545

    mail in .env

--- project/settings.py
+++ project/settings.py
@@ -169,10 +169,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
 
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER='kusumshrestha367@gmail.com'  
-EMAIL_HOST_PASSWORD='pyht bmcn qazd lhqs'  
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))  # Default to 587 if not found
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
